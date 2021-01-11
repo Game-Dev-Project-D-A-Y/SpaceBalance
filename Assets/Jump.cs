b61d9df9a2c32ca6e8c53c4dce067d5c563d0e8e
doviec <doviechitiz@gmail.com>
2021-01-11 17:04:21 +0200

    jump

---
+++ Assets/Jump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5f;
+    private Rigidbody rigidbody;
+    private bool onGround = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            rigidbody.AddForce(Vector3.up * speed, ForceMode.Impulse);
+            onGround = false;
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Base")
+        {
+            onGround = true;
+
+        }
+    }
+
+
+}
