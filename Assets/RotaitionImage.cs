df65a37111ab2d54867fb240665ecc065eef31b3
doviec <doviechitiz@gmail.com>
2021-01-11 02:06:46 +0200

    design

--- Assets/RotaitionImage.cs
+++ Assets/RotaitionImage.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotaition : MonoBehaviour
+public class RotaitionImage : MonoBehaviour
 {
     [SerializeField]
     float rotate = 0.6f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       transform.Rotate(new Vector3(0, rotate,0 ));
+        transform.Rotate(new Vector3(0, 0, rotate));
 
-       
-        
+
+
     }
 }
