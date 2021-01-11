a4581874ddd30529e691891383f5eb628ad750ec
doviec <doviechitiz@gmail.com>
2021-01-11 02:46:57 +0200

    intro

---
+++ Assets/InfoMovment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoMovment : MonoBehaviour
+{
+    [SerializeField]
+    float scrollSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        Vector3 localVectorUp = transform.TransformDirection(0, 1, 0);
+
+        pos += localVectorUp * scrollSpeed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
