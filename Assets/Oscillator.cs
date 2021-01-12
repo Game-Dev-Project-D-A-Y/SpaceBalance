291396757ba94cc8b0cea5c2f85fc4feb9b007d4
doviec <doviechitiz@gmail.com>
2021-01-12 03:34:21 +0200

    design

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField]
+    float timeCounter = 0;
+
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    float width;
+
+    [SerializeField]
+    float heigt;
+
+    float x;
+    float y;
+
+    [SerializeField]
+    float heightOfPlanet = 5;
+
+    [SerializeField]
+    float z = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime * speed;
+
+        x = Mathf.Cos(timeCounter) * width;
+        y = heightOfPlanet + Mathf.Sin(timeCounter) * heigt;
+        transform.position = new Vector3(x, y, z);
+    }
+}
